39a8f49b3c1713c6da179040360ffb95e7ad3c5c
AJLAN <107946506+AJLAN0@users.noreply.github.com>
2024-03-11 19:52:19 +0300

    Update Scripting.js

--- Scripting.js
+++ Scripting.js
@@ -19,14 +19,14 @@ $(document).ready(function(){
 
 
     var typed = new Typed(".typing",{
-        strings: ["Developer", "Designer", "Freelancer", "Programmer", "Accountant"],
+        strings: ["Developer", "Designer", "Freelancer", "Programmer", "UI / UX Designer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true,
     });
 
     var typed = new Typed(".typing-2",{
-        strings: ["Developer", "Designer", "Freelancer", "Programmer", "Accountant"],
+        strings: ["Developer", "Designer", "Freelancer", "Programmer", "UI / UX Designer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true,
